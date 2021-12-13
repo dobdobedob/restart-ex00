af72560505755a1d2c3c57fce3dd2dec2d67f832
dobdobedob <smkdkd1993@gmail.com>
2021-12-13 09:11:44 +0900

    이..이게머여

---
+++ src/main/java/org/zerock/controller/p05controller/bean/Bean11.java
@@ -0,0 +1,9 @@
+package org.zerock.controller.p05controller.bean;
+
+import lombok.Data;
+
+@Data
+public class Bean11 {
+	private Integer customerID;
+	private String  customerName;
+}
---
+++ src/main/java/org/zerock/mapper/project1/BoardMapper.java
@@ -0,0 +1,23 @@
+package org.zerock.mapper.project1;
+
+import java.util.List;
+
+import org.zerock.domain.project1.BoardVO;
+
+public interface BoardMapper {
+
+	// 모든 게시물 조회
+	public List<BoardVO> getList();
+	
+	// get generated key
+	public int insert(BoardVO board); 
+	
+	// id(pk)로 하나의 게시물 조회
+	public BoardVO read(Integer id); 
+	
+	// id(pk)로 하나의 게시물 조회
+	public int delete(Integer id); 
+	
+	// 하나의 게시물 수정
+	public int update(BoardVO board); 
+}
